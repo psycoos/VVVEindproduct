7ef82815d50b44a289b4a23adb2539baf7376507
Psycoos <coospot4@gmail.com>
2017-11-21 12:05:22 +0100

    initialize new stampcard on homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { StampCard } from '../../models/stamp-card/stamp-card.model';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  stampCard: StampCard = {
+    Leeuwarden: false,
+    Sneek: false,
+    Ijlst: false,
+    Sloten: false,
+    Stavoren: false,
+    Hindeloopen: false,
+    Workum: false,
+    Bolsward: false,
+    Harlingen: false,
+    Franeker: false,
+    Dokkum: false
+  }
 
   constructor(public navCtrl: NavController) {
 
