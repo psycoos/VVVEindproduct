9b0e8b2f1521db9cd95a8308ba6e733596b66021
RenzeD98 <renze.d98@gmail.com>
2017-12-05 10:06:44 +0100

    changed grid to six columns

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,12 +17,12 @@ export class HomePage {
     { id: 3, name: "IJlst", stamped: false, checked: "ijlst/ijlst_2.svg", unchecked: "ijlst/ijlst_1.svg"},
     { id: 4, name: "Sneek", stamped: true, checked: "sneek/sneek_2.svg", unchecked: "sneek/sneek_1.svg"},
     { id: 5, name: "Stavoren", stamped: false, checked: "stavoren/stavoren_2.svg", unchecked: "stavoren/stavoren_1.svg"},
-    { id: 6, name: "Hindeloopen", stamped: false, checked: "hindeloopen/hindeloopen_2.svg", unchecked: "hindeloopen/hindeloopen_1.svg"},
+    { id: 6, name: "Hindeloopen", stamped: true, checked: "hindeloopen/hindeloopen_2.svg", unchecked: "hindeloopen/hindeloopen_1.svg"},
     { id: 7, name: "Workum", stamped: false, checked: "workum/workum_2.svg", unchecked: "workum/workum_1.svg"},
     { id: 8, name: "Bolsward", stamped: true, checked: "bolsward/bolsward_2.svg", unchecked: "bolsward/bolsward_1.svg"},
     { id: 9, name: "Harlingen", stamped: false, checked: "harlingen/harlingen_2.svg", unchecked: "harlingen/harlingen_1.svg"},
     { id: 10, name: "Franeker", stamped: false, checked: "franeker/franeker_2.svg", unchecked: "franeker/franeker_1.svg"},
-    { id: 11, name: "Dokkum", stamped: false, checked: "dokkum/dokkum_2.svg", unchecked: "dokkum/dokkum_1.svg"}
+    { id: 11, name: "Dokkum", stamped: true, checked: "dokkum/dokkum_2.svg", unchecked: "dokkum/dokkum_1.svg"}
   ];  
 
 }
