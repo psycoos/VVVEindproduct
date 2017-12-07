b6b4775f178f50676f801924a8b8398f4a30b635
RenzeD98 <renze.d98@gmail.com>
2017-12-07 11:28:09 +0100

    added markers for every city

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -24,13 +24,49 @@ export class MapsPage {
     // location - lat long
     const location = new google.maps.LatLng(53.164164, 5.781754);
 
+    //location for every city
+    const Leeuwarden = new google.maps.LatLng(53.1980506, 5.7919859,21);
+    const Dokkum = new google.maps.LatLng(53.3271086, 5.9986493,21);
+    const Sloten = new google.maps.LatLng(52.8947695, 5.6456006,21);
+    const IJlst = new google.maps.LatLng(53.0110137, 5.6218303,21);
+    const Sneek = new google.maps.LatLng(53.0322788, 5.6632035,21);
+    const Stavoren = new google.maps.LatLng(52.8865688,5.3579681,21);
+    const Hindeloopen = new google.maps.LatLng(52.9424502, 5.4024287,21);
+    const Workum = new google.maps.LatLng(52.9794418, 5.4438464,21);
+    const Bolsward = new google.maps.LatLng(53.0618983, 5.5228519,21);
+    const Harlingen = new google.maps.LatLng(53.1747075, 5.4149199,21);
+    const Franeker = new google.maps.LatLng(53.185715, 5.5455836,21);
+    
+    
     // Map options
     const options = {
       center: location,
       zoom: 10,
+      streetViewControl: false,
+      mapTypeControl: false,
+      fullscreenControl: false
     }
 
     this.map = new google.maps.Map(this.mapRef.nativeElement, options)
+
+    this.addMarker(Leeuwarden, this.map);
+    this.addMarker(Dokkum, this.map);
+    this.addMarker(Sloten, this.map);
+    this.addMarker(IJlst, this.map);
+    this.addMarker(Sneek, this.map);
+    this.addMarker(Stavoren, this.map);
+    this.addMarker(Hindeloopen, this.map);
+    this.addMarker(Workum, this.map);
+    this.addMarker(Bolsward, this.map);
+    this.addMarker(Harlingen, this.map);
+    this.addMarker(Franeker, this.map);
+  }
+
+  addMarker (position, map) {
+    new google.maps.Marker({
+      position,
+      map
+    })
   }
 
 }
