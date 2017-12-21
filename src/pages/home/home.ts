68669076c25adfcb9c8ee27e5f6f2fe2be2f3b1f
Psycoos <coospot4@gmail.com>
2017-12-21 11:50:03 +0100

    comment and refactor home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,8 +4,6 @@ import { City } from '../../models/stamp-card/stamp-card.model';
 import { Storage } from '@ionic/storage'
 import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 
-import { QrCodeProvider } from '../../providers/qr-code/qr-code'
-
 
 @Component({
   selector: 'page-home',
@@ -39,56 +37,53 @@ export class HomePage {
       trueimage: "sloten/sloten_2.svg",
       falseimage: "sloten/sloten_1.svg"
     }
-  ]
+  ] //deze array wordt gebruikt om de stampcard te initialiseren
 
-  public curStamp = [];
+  public curStamp = []; //Deze array loopt altijd synchroon met de localstorage
 
   constructor(
     public navCtrl: NavController,
     public storage: Storage,
-    private barcode: BarcodeScanner,
-    private qrcodeProvider: QrCodeProvider,
+    private barcode: BarcodeScanner
   ) {
+    //zet de stempelkaart in de array als er nog geen is, update anders curStamp met localstorage
     storage.get('stampcard').then((kaart) => {
       if (kaart === null || kaart === undefined) {
         //geen stempelkaart gevonden in de local storage, nieuwe toevoegen
         storage.set('stampcard', this.stampCard);
         this.curStamp = this.stampCard;
       } else  
-        this.curStamp = kaart; //Kaart uit de local storage
+        this.curStamp = kaart; //kaart uit de localstorage
     });
   }
 
-  //Clear storage
+  //clear localstorage
   clearStorage() {
     console.log("CLEAR");
     this.storage.clear();
   }
 
+  //scan qr-code
   scan() {
     this.barcode.scan().then((barcodeData) => {
-      this.checkValue(barcodeData.text);
+      this.checkValue(barcodeData.text);  //roep checkValue aan op het resultaat van de scan
     }, (err) => {
       // error
       alert(err);
     });
   }
 
+
+  //verandert de stempel state van de gescande stad in true
   checkValue(scanResult) {
     this.storage.get('stampcard').then((kaart) => {
       for (var i in kaart) {
-        if (kaart[i].name === scanResult) {
-          kaart[i].value = true;
-          this.curStamp = kaart;
-          this.storage.set('stampcard', kaart);
+        if (kaart[i].name === scanResult) {//check of de scan overeenkomt met een stad in de stempelkaart
+          kaart[i].value = true;//set de value
+          this.curStamp = kaart;//update curStamp
+          this.storage.set('stampcard', kaart);//update localstorage zodat deze gelijk is aan curStamp
         }
       }
     });
   }
-
-  latenzien() {
-  //  this.dingendoen("Leeuwarden");
-  }
-
-  ionViewDidLoad() {}
 }
