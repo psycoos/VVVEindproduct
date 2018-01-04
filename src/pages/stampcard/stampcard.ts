02c0c9bcc9e2f46202bdf369168a0c8646f7b4c2
jurjenborkent <jurjenborkent@gmail.com>
2018-01-04 09:47:35 +0100

    Fixed bug that crashed the qr scanner in the app

--- src/pages/stampcard/stampcard.ts
+++ src/pages/stampcard/stampcard.ts
@@ -9,6 +9,7 @@ import { Storage } from '@ionic/storage';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 import { stampService } from '../../providers/stamp-service';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 
 @IonicPage()
@@ -17,11 +18,19 @@ import { stampService } from '../../providers/stamp-service';
   templateUrl: 'stampcard.html',
 })
 
-export class StampcardPage {
+export class StampcardPage implements AfterViewInit{
+
+  buttonDisabled: boolean;
+
+  ngAfterViewInit(): void {
+    this.buttonDisabled = false;
+  }
   
   tab1Root = MapsPage;
   tab2Root = HomePage;
   tab3Root = UserPage;
+
+  
 
   public stampcarousel = [];
 
@@ -47,24 +56,28 @@ export class StampcardPage {
     const browser = this.iab.create('https://www.google.com/maps/dir/?api=1&destination=Leeuwarden&travelmode=walking');
   }
 
-  scan() {
-    this.barcode.scan().then((barcodeData) => {
+    scan() {
+      this.buttonDisabled = true;
+     this.barcode.scan().then((barcodeData) => {
       this.checkValue(barcodeData.text);  //roep checkValue aan op het resultaat van de scan
-    }, (err) => {
-      // error
+    }   , (err) => { // error
       alert(err);
-    });
-  }
+    }
+   );
+   }
+
 
 
   //verandert de stempel state van de gescande stad in true
   checkValue(scanResult) {
+    this.buttonDisabled = false;
     this.storage.get('stampcard').then((kaart) => {
       for (var i in kaart) {
         if (kaart[i].name === scanResult) {//check of de scan overeenkomt met een stad in de stempelkaart
           kaart[i].value = true;//set de value
           this.stampService.stamp = kaart;//update stamp
            this.storage.set('stampcard', kaart);//update localstorage zodat deze gelijk is aan curStamp
+           
         }
       }
     });
